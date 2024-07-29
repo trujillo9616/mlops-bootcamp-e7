411e1eba50bc61c0424a083ac5ebd45e41dbf5b4
trujillo96 <adrian.trujillo@salesloft.com>
2024-07-29 11:20:16 -0600

    Add bucket name to s3 module output

--- terraform/modules/s3/output.tf
+++ terraform/modules/s3/output.tf
@@ -2,3 +2,8 @@ output "s3_bucket_id" {
   value       = aws_s3_bucket.bucket.id
   description = "The ID of the S3 bucket"
 }
+
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.bucket.bucket
+  description = "The name of the S3 bucket"
+}
