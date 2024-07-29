db8ef76a0932ce3f96a392cda0dc7c9e19dd5f7a
trujillo96 <adrian.trujillo@salesloft.com>
2024-07-29 11:09:35 -0600

    Update format

--- terraform/infra/users.tf
+++ terraform/infra/users.tf
@@ -1,10 +1,10 @@
 module "dev_users" {
   source = "../modules/dev_users"
-  users = ["ricardo-diaz", "hugo-ramirez"]
+  users  = ["ricardo-diaz", "hugo-ramirez"]
   policy_statements = [
     {
-      effect    = "Allow"
-      actions   = ["s3:*"]
+      effect  = "Allow"
+      actions = ["s3:*"]
       resources = [
         "arn:aws:s3:::${module.s3_bucket_datalake.bucket_name}",
         "arn:aws:s3:::${module.s3_bucket_datalake.bucket_name}/*"
