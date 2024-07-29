f39c9a1858b7020b9150ca5f08951ab34ef4ab8d
trujillo96 <adrian.trujillo@salesloft.com>
2024-07-28 22:34:11 -0600

    Add DynamoDB module

---
+++ terraform/modules/dynamodb/variables.tf
@@ -0,0 +1,9 @@
+variable "table_name" {
+  description = "The name of the DynamoDB table"
+  type        = string
+}
+
+variable "hash_key" {
+  description = "The name of the hash key"
+  type        = string
+}
